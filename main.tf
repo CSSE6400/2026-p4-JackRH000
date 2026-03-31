bfadaf23e190ca2cd59f45d6467df3e4d59a0966
jack <jack64rh@gmail.com>
2026-03-31 13:02:27 +1000

    Completed practical

---
+++ main.tf
@@ -0,0 +1,59 @@
+terraform { 
+    required_providers { 
+        aws= { 
+            source = "hashicorp/aws" 
+            version= "~> 6.0" 
+        } 
+    } 
+} 
+
+provider "aws" { 
+    region= "us-east-1" 
+    shared_credentials_files= ["./credentials"] 
+    default_tags { 
+        tags = { 
+            Environment="Dev" 
+            Course= "CSSE6400" 
+            StudentID="47498527" 
+        } 
+    } 
+}
+
+resource "aws_instance" "hextris-server" { 
+    ami= "ami-0c421724a94bba6d6" 
+    user_data=file("./serve-hextris.sh")
+    instance_type="t2.micro"
+    security_groups = [aws_security_group.hextris-server.name] 
+    key_name= "vockey" 
+    tags= { 
+        Name= "hextris" 
+    } 
+}
+
+resource"aws_security_group" "hextris-server" { 
+    name= "hextris-server" 
+    description="HextrisHTTP andSSHaccess" 
+   
+    ingress { 
+        from_port= 80 
+        to_port= 80 
+        protocol= "tcp" 
+        cidr_blocks=["0.0.0.0/0"] 
+    } 
+    ingress { 
+        from_port= 22 
+        to_port= 22 
+        protocol= "tcp" 
+        cidr_blocks=["0.0.0.0/0"] 
+    } 
+    egress { 
+        from_port= 0 
+        to_port= 0 
+        protocol= "-1" 
+        cidr_blocks=["0.0.0.0/0"] 
+    } 
+}
+
+output "hextris-url" { 
+    value = "http://${aws_instance.hextris-server.public_ip}/" 
+}
